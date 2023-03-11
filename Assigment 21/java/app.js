4053bc07c58492e747425b8d3bc4035013267763
M-Muiz <122803576+M-Muiz@users.noreply.github.com>
2023-03-11 14:59:57 +0500

    Add files via upload

---
+++ Assigment 21/java/app.js
@@ -0,0 +1,187 @@
+Q:1
+
+// let a = prompt("Enter your first name");
+// let b = prompt("Enter your Last name");
+
+// let c = a + b;
+
+// alert("Good Morning"  + " " + c);
+
+
+Q:2
+
+// let a = prompt("Enter your favorite mobile model");
+
+// document.write("My favorite phone is: " + a) ;
+
+
+// document.write("<br> Lenght of string:" + [a.length])
+
+
+Q:3
+
+// let index = ("Pakistani");
+
+// let a = index.indexOf("n");
+
+// document.write("Index of 'n': " + a);
+
+Q:4 
+
+// let lastindex = ("Hello World");
+
+// let a = lastindex.lastIndexOf("l");
+
+// document.write("Last Index of 'l': " + a)
+
+Q:5
+
+// let index = ("Pakistani");
+
+// let a = index.indexOf();
+
+// document.write("Character at index 3: " + a);
+
+
+Q:6
+
+/*Using ConCat  Method */
+
+// let a = prompt("Enter your first name");
+// let b = prompt("Enter your Last name");
+
+// let c = a.concat(b);
+
+// alert("Good Morning"  + " " + c);
+
+
+Q:7
+
+// let a = ('Hyderabad');
+// let b = ("Islam");
+
+// let c =a.slice(5); 
+
+// let d = b + c
+
+// document.write("City:" + a);
+// document.write("<br>After Replacement: " + d);
+
+
+Q:8
+
+// let a = ("472");
+
+// document.write("Value: " + a)
+// document.write("<br>Type: " + typeof(a))
+
+// let b = Number.parseInt(a);
+// document.write("<br>Value: " + b)
+// document.write("<br>Type: " + typeof(b))
+
+
+Q:10
+
+// let cap = prompt("Emter your word");
+
+// let a = cap.toUpperCase();
+
+// document.write("User Input: " + cap);
+// document.write("<br>Upper Case: " + a);
+
+Q:11
+
+// let title = prompt("Enter Your Title");
+
+// let b =title.slice(1)
+
+// let c = b.toUpperCase();
+
+// console.log(title)
+// console.log(b)
+// console.log(c)
+
+
+Q:12
+
+// var num = 35.36;
+
+// num.splice(2)
+
+// console.log(num)
+
+Q:13
+
+// let a = prompt("Enter a password");
+
+// let b = a.split("");
+// // console.log(b)
+
+
+// for(var i=0; i < b.length; i++){
+
+
+//     if(b[i] == "!" || b[i] == "." || b[i] == "," || b[i] == "@"){
+//         alert("Enter a valid password")
+//     }
+    
+    
+// }
+
+
+Q:14
+
+// let a = prompt("Welcome to Bakers, What do you want to order")
+
+// let b = ["cake", "apple pie", "cookie", "chips", "patties"];
+ 
+// for(var i=0; i<a.length; i++){
+
+    
+    
+//     if(b[i] == a){
+//         console.log( a + " is avaliable at index " + i + " in our bakery")
+//     }    
+// }
+
+Q:15
+
+// let a = prompt("Create a Password");
+
+// let b = 
+
+
+Q:16
+
+// var un = "University of Karachi";
+
+// var a = un.split("");
+
+// console.log(a);
+
+
+Q:17
+
+// let a = prompt("Enter anything to find the last character","Pakistan");
+
+// let b = a.slice(-1);
+
+// alert (b);
+
+Q:18
+
+// let str = ("The quick brown fox jumps over the lazy dog");
+
+// let count = 0;
+// let a = str.split(" ");
+
+
+
+
+// for(var i =0; i<a.length; i++){
+//     if(a[i].toLowerCase() == "the"){
+//         count++;
+//     }
+// }
+
+// document.write("There are " + count + " occurence of word 'the'");
